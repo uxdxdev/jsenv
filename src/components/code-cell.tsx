66d5bc8b5d7eac98b9070f5960785f2ba3f1b1df
David Morton <davidmortonmail@gmail.com>
2023-02-09 23:52:40 +0000

    updated starting example

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -5,9 +5,32 @@ import bundle from '../bundler';
 import Resizable from './resizable';
 import './code-cell.css';
 
-const REACT_APP_INIT = `import React from 'react@^18';
-import ReactDOM from 'react-dom@^18';
-import styled from 'styled-components';
+const REACT_APP_INIT = `import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+// fonts
+import WebFont from 'webfontloader';
+// css-in-js
+import styled, { ThemeProvider } from 'styled-components';
+// Radix https://www.radix-ui.com/
+import { blue, green } from '@radix-ui/colors';
+import { FaceIcon } from '@radix-ui/react-icons';
+
+// styled-components global styles don't work in the preview iframe
+// so you need to set styles on the body directly
+document.body.style = \`margin: 0; font-family: 'Atkinson Hyperlegible';\`;
+
+WebFont.load({
+  google: {
+    families: ['Atkinson Hyperlegible'],
+  },
+});
+
+const theme = {
+  colors: {
+    ...blue,
+    ...green,
+  },
+};
 
 const Title = styled.h1\`
   font-size: 1.5em;
@@ -15,16 +38,65 @@ const Title = styled.h1\`
   color: palevioletred;
 \`;
 
-const Wrapper = styled.section\`
+const Wrapper = styled.section\`  
   padding: 4em;
   background: papayawhip;
 \`;
 
+const Counter = styled.div\`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+\`;
+
+const CountValue = styled.div\`
+  font-size: 2em;
+\`;
+
+const Button = styled.button\`
+  font-family: inherit;
+  background-color: \${(props) => props.theme.colors.blue4};
+  color: \${(props) => props.theme.colors.blue11};
+  border-color: \${(props) => props.theme.colors.blue7};
+  &:hover {
+    background-color: \${(props) => props.theme.colors.green5};
+    border-color: \${(props) => props.theme.colors.green8};
+  }
+\`;
+
+const useCounter = () => {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount(count + 1);
+  }
+  return {
+    count,
+    increment,
+  };
+};
+
+const Count = () => {
+  const { count, increment } = useCounter();
+  return (
+    <Counter>
+      <CountValue>{count}</CountValue>
+      <Button onClick={() => increment()}>Click me</Button>
+    </Counter>
+  );
+};
+
 const App = () => {
   return (
-    <Wrapper>
-      <Title>Hello World!</Title>
-    </Wrapper>
+    <ThemeProvider theme={theme}>
+      <Wrapper>
+        <Title>
+          Hello World!
+          <FaceIcon />
+        </Title>
+        <Count />
+      </Wrapper>
+    </ThemeProvider>
   );
 };
 
--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -8,9 +8,7 @@ interface PreviewProps {
 
 const html = `
     <html>
-      <head>
-        <style>html { background-color: white; }</style>
-      </head>
+      <head></head>
       <body>
         <div id="root"></div>
         <script>
